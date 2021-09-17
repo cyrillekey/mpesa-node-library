bbd3800c495f797e7a1755d96faa52418f7fb73d
dannysofftie <dankim761@gmail.com>
2021-09-17 10:40:13 +0300

    Refactor types

--- tests/helpers/instance.js
+++ tests/helpers/instance.js
@@ -1,25 +1,17 @@
-const path = require('path')
-const Mpesa = require('../.././src/m-pesa')
-require('dotenv').config({path: path.resolve('tests/helpers/tests.env')})
-const {
-  CONSUMER_KEY,
-  CONSUMER_SECRET,
-  SHORTCODE,
-  INITIATOR_NAME,
-  LIPA_NA_MPESA_SHORTCODE,
-  LIPA_NA_MPESA_SHORTPASS,
-  SECURITY_CREDENTIAL
-} = process.env
-const testInstance = new Mpesa({
+const path = require('path');
+const Mpesa = require('../../src/index');
+require('dotenv').config({ path: path.resolve('tests/helpers/tests.env') });
+const { CONSUMER_KEY, CONSUMER_SECRET, SHORTCODE, INITIATOR_NAME, LIPA_NA_MPESA_SHORTCODE, LIPA_NA_MPESA_SHORTPASS, SECURITY_CREDENTIAL } = process.env;
+const testInstance = new Mpesa.default({
   consumerKey: CONSUMER_KEY,
   consumerSecret: CONSUMER_SECRET,
   environment: 'sandbox',
-  shortCode: SHORTCODE,
+  shortCode: Number(SHORTCODE),
   initiatorName: INITIATOR_NAME,
   lipaNaMpesaShortCode: LIPA_NA_MPESA_SHORTCODE,
   lipaNaMpesaShortPass: LIPA_NA_MPESA_SHORTPASS,
   securityCredential: SECURITY_CREDENTIAL,
-  certPath: path.resolve('keys/sandbox-cert.cer')
-})
+  certPath: path.resolve('keys/sandbox-cert.cer'),
+});
 
-module.exports = testInstance
+module.exports = testInstance;
--- tests/unit/index.test.js
+++ tests/unit/index.test.js
@@ -1,54 +1,33 @@
-const expect = require('expect.js')
-const Mpesa = require('../.././src/m-pesa')
-const instance = new Mpesa({ consumerKey: 'test', consumerSecret: 'test' })
-const {
-  accountBalance,
-  b2b,
-  b2c,
-  c2bRegister,
-  c2bSimulate,
-  lipaNaMpesaOnline,
-  lipaNaMpesaQuery,
-  oAuth,
-  reversal,
-  transactionStatus
-} = instance
+const expect = require('expect.js');
+const Mpesa = require('../../src');
+const instance = new Mpesa.default({ consumerKey: 'test', consumerSecret: 'test' });
+const { accountBalance, b2b, b2c, c2bRegister, c2bSimulate, lipaNaMpesaOnline, lipaNaMpesaQuery, oAuth, reversal, transactionStatus } = instance;
 
 describe('All Methods are Callble', function () {
-  [
-    accountBalance,
-    b2b,
-    b2c,
-    c2bRegister,
-    c2bSimulate,
-    lipaNaMpesaOnline,
-    lipaNaMpesaQuery,
-    oAuth,
-    reversal,
-    transactionStatus ].map(f => {
+  [accountBalance, b2b, b2c, c2bRegister, c2bSimulate, lipaNaMpesaOnline, lipaNaMpesaQuery, oAuth, reversal, transactionStatus].map(f => {
     it(f.name, function () {
-      expect(typeof f).to.be('function')
-    })
-  })
-})
+      expect(typeof f).to.be('function');
+    });
+  });
+});
 
 describe('C2B', function () {
-  const productionInstance = new Mpesa({
+  const productionInstance = new Mpesa.default({
     consumerKey: 'test',
     consumerSecret: 'test',
-    environment: 'production'
-  })
+    environment: 'production',
+  });
 
   it('should throw error in production', function () {
-    let threwError = false
-    
+    let threwError = false;
+
     try {
-      productionInstance.c2bSimulate()
+      productionInstance.c2bSimulate(null);
     } catch (e) {
-      threwError = true
-      expect(e.message).to.be('Cannot call C2B simulate in production.')
+      threwError = true;
+      expect(e.message).to.be('Cannot call C2B simulate in production.');
     } finally {
-      expect(threwError).to.be(true)
+      expect(threwError).to.be(true);
     }
-  })
-})
+  });
+});
