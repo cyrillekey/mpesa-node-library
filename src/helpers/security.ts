c94c9a7b585c6ecc6bcf1b30d51f9fc05b47aa67
dannysofftie <dankim761@gmail.com>
2021-09-27 09:59:53 +0300

    Prefer using global certificate path and operator password

--- src/helpers/security.ts
+++ src/helpers/security.ts
@@ -2,11 +2,11 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as crypto from 'crypto';
 
-export default (certPath: string, shortCodeSecurityCredential: string) => {
-  if (!certPath) throw new Error(`Error! Certificate path is required!`);
-  if (!shortCodeSecurityCredential) throw new Error(`Error! Operator password is required!`);
-  const bufferToEncrypt = Buffer.from(shortCodeSecurityCredential);
-  const data = fs.readFileSync(path.resolve(certPath));
+export default function () {
+  if (!this.certPath) throw new Error(`Error! Certificate path is required!`);
+  if (!this.securityCredential) throw new Error(`Error! Operator password is required!`);
+  const bufferToEncrypt = Buffer.from(this.securityCredential);
+  const data = fs.readFileSync(path.resolve(this.certPath));
   const privateKey = String(data);
   const encrypted = crypto.publicEncrypt(
     {
@@ -15,6 +15,5 @@ export default (certPath: string, shortCodeSecurityCredential: string) => {
     },
     bufferToEncrypt
   );
-  const securityCredential = encrypted.toString('base64');
-  return securityCredential;
-};
+  return encrypted.toString('base64');
+}
